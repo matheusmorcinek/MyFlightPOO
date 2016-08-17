a234fce6b1a4150d3c0db4d0968b55e05a4d35b3
Matheus <Matheus@192.168.25.6>
2016-08-17 02:12:34 -0300

    GerenciadorAeroportos - Added (adicionar, listarTodos, buscarPorCodigo)

--- MyFlight-base/src/pucrs/myflight/modelo/GerenciadorAeroportos.java
+++ MyFlight-base/src/pucrs/myflight/modelo/GerenciadorAeroportos.java
@@ -1,8 +1,37 @@
 package pucrs.myflight.modelo;
 
+import java.util.ArrayList;
 
 public class GerenciadorAeroportos {
 
+	private ArrayList<Aeroporto> aeroportos;
+	
+	public GerenciadorAeroportos(){
+		aeroportos = new ArrayList<>();
+	}
+	
+	public void adicionar(Aeroporto aero){
+		aeroportos.add(aero);
+	}
+	
+	public ArrayList<Aeroporto> listarTodos(){
+		ArrayList<Aeroporto> nova = new ArrayList<>();
+		
+		for(Aeroporto a : aeroportos){
+			nova.add(a);
+		}
+		
+		return nova;
+	}
+	
+	public Aeroporto buscarPorCodigo(String cod){
+		for(Aeroporto a : aeroportos){
+			if(cod.equals(a.getCodigo())){
+				return a;
+			}
+		}
+		return null;
+	}
 	
 	
 }
