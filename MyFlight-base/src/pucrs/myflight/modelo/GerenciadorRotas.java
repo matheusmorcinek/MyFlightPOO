d82c9fb755c658ceaa8f6de94911da8592bf7b17
Matheus <Matheus@192.168.25.6>
2016-08-17 15:25:19 -0300

    GerenciadorRotas - Added (adicionar, listarTodas, buscarPorOrigem)

--- MyFlight-base/src/pucrs/myflight/modelo/GerenciadorRotas.java
+++ MyFlight-base/src/pucrs/myflight/modelo/GerenciadorRotas.java
@@ -1,5 +1,35 @@
 package pucrs.myflight.modelo;
 
+import java.util.ArrayList;
+
 public class GerenciadorRotas {
+
+	private ArrayList<Rota> rotas;
+
+	public GerenciadorRotas() {
+		rotas = new ArrayList<>();
+	}
+
+	public void adicionar(Rota rota) {
+		rotas.add(rota);
+	}
+
+	public ArrayList<Rota> listarTodas() {
+		ArrayList<Rota> nova = new ArrayList<>();
+
+		for (Rota r : rotas) {
+			nova.add(r);
+		}
+		return nova;
+	}
+
+	public Rota buscarPorOrigem(Aeroporto aero) {
+		for (Rota r : rotas) {
+			if (aero.equals(r.getOrigem())) {
+				return r;
+			}
+		}
+		return null;
+	}
 
 }
