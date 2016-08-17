4886984f92759b422119d0015b84a9d801ddbb38
Matheus <Matheus@192.168.25.6>
2016-08-17 16:26:21 -0300

    GerenciadorVoos - Added (adicionar, listarTodos, buscarData)

--- MyFlight-base/src/pucrs/myflight/modelo/GerenciadorVoos.java
+++ MyFlight-base/src/pucrs/myflight/modelo/GerenciadorVoos.java
@@ -1,5 +1,40 @@
 package pucrs.myflight.modelo;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
 public class GerenciadorVoos {
+
+	private ArrayList<Voo> voos;
+
+	public GerenciadorVoos() {
+		voos = new ArrayList<>();
+	}
+
+	public void adicionar(Voo voo) {
+		voos.add(voo);
+	}
+
+	public ArrayList<Voo> listarTodos() {
+		ArrayList<Voo> nova = new ArrayList<>();
+
+		for (Voo v : voos) {
+			nova.add(v);
+		}
+		return nova;
+	}
+
+	public ArrayList<Voo> buscarData(LocalDateTime data) {
+		ArrayList<Voo> nova = new ArrayList<>();
+
+		for (Voo v : voos) {
+
+			if (data.equals(v.getDatahora())) {
+				nova.add(v);
+			}
+
+		}
+		return nova;
+	}
 
 }
