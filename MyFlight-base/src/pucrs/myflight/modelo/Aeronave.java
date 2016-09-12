e8dc643737d9b599e1f884a8b0707bea2813f1a0
16111201 <matheusmorcinek@gmail.com>
2016-09-12 18:35:12 -0300

    Aeronave - Added (capacidade)

--- MyFlight-base/src/pucrs/myflight/modelo/Aeronave.java
+++ MyFlight-base/src/pucrs/myflight/modelo/Aeronave.java
@@ -3,37 +3,42 @@ package pucrs.myflight.modelo;
 public class Aeronave implements Imprimivel, Contavel, Comparable<Aeronave> {
 	private String codigo;
 	private String descricao;
+	private int capacidade;
 	private static int totalAeronaves = 0;
-	
-	public Aeronave(String codigo, String descricao) {
+
+	public Aeronave(String codigo, String descricao, int capacidade) {
 		this.codigo = codigo;
 		this.descricao = descricao;
+		this.capacidade = capacidade;
 		totalAeronaves++;
 	}
-	
-	@Override
-	public int totalAeronaves() {
-		return totalAeronaves;
-	}
-	
+
 	public String getCodigo() {
 		return codigo;
 	}
-	
+
 	public String getDescricao() {
 		return descricao;
 	}
 
+	public int getCapacidade() {
+		return capacidade;
+	}
+
+	@Override
+	public int totalAeronaves() {
+		return totalAeronaves;
+	}
+
 	@Override
 	public void imprimir() {
-		
+
 		System.out.println(codigo + " - " + descricao);
-		
+
 	}
 
 	@Override
 	public int compareTo(Aeronave o) {
-		// TODO Auto-generated method stub
 		return codigo.compareTo(o.codigo);
 	}
 }
